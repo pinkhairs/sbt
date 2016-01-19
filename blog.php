94f7f1fc57def9da35bff6505685eab7fb8a76f9
me@diana.nu <diana@Dianas-MacBook-Air.local>
2016-01-18 19:25:11 -0800

    Blog is now desktop-friendly

--- blog.php
+++ blog.php
@@ -11,7 +11,7 @@
  <div class="row">
  	<div class="small-12 columns">
 		<div class="section pt0 pb0 text-center">
-			<div class="dropdown-container">
+			<div class="dropdown-container mobile-only">
 				<div class="dropdown-label standalone-dropdown ">Most Recent</div>
 				<ul class="dropdown standalone-dropdown-options short-standalone">
 					<li><a href="#">Popular</a></li>
@@ -21,35 +21,84 @@
 					<li><a href="#">Technology</a></li>
 				</ul>
 			</div>
+			<div class="dropdown-container desktop-only">
+				<ul class="dropdown standalone-dropdown-options short-standalone">
+					<li class="active"><a href="#" class="reverse">Recent</a></li>
+					<li><a href="#">Popular</a></li>
+					<li><a href="#">Design</a></li>
+				</ul>
+			</div>
 		</div>
 	</div>
 </div>
  <div class="row">
  	<div class="small-12 columns">
-		<div class="section pb0">
-			<div class="post">
-				<div class="post-container">
-					<p><img src="images/post1.jpg" alt=""></p>
-					<p class="lead"><a href="#">Career</a></p>
-					<h4><a href="#">Confessions from Recruiters for Year 2015</a></h4>
-				</div><!--
-				--><a href="#" class="full-width-button read-button">Read On</a>
+		<div class="section pb0 pt0">
+			<div class="row">
+				<div class="small-12 large-4 columns">
+					<div class="post">
+						<div class="post-container">
+							<p><img src="images/post1.jpg" alt=""></p>
+							<p class="lead lead-small"><a href="#">Career</a></p>
+							<h4><a href="#">Confessions from Recruiters for Year 2015</a></h4>
+						</div><!--
+						--><a href="#" class="full-width-button read-button">Read On</a>
+					</div>
+				</div>
+				<div class="small-12 large-4 columns">
+					<div class="post">
+						<div class="post-container">
+							<p><img src="images/post2.jpg" alt=""></p>
+							<p class="lead lead-small"><a href="#">Design</a></p>
+							<h4><a href="#">How to Nab Those Exciting Design Jobs</a></h4>
+						</div><!--
+						--><a href="#" class="full-width-button read-button">Read On</a>
+					</div>
+				</div>
+				<div class="small-12 large-4 columns">
+					<div class="post">
+						<div class="post-container">
+							<p><img src="images/post3.jpg" alt=""></p>
+							<p class="lead lead-small"><a href="#">Electric Vehicle</a></p>
+							<h4><a href="#">What You Need to Know to Get Into Electric Cars</a></h4>
+						</div><!--
+						--><a href="#" class="full-width-button read-button">Read On</a>
+					</div>
+				</div>
 			</div>
-			<div class="post">
-				<div class="post-container">
-					<p><img src="images/post2.jpg" alt=""></p>
-					<p class="lead"><a href="#">Design</a></p>
-					<h4><a href="#">How to Nab Those Exciting Design Jobs</a></h4>
-				</div><!--
-				--><a href="#" class="full-width-button read-button">Read On</a>
-			</div>
-			<div class="post">
-				<div class="post-container">
-					<p><img src="images/post3.jpg" alt=""></p>
-					<p class="lead"><a href="#">Electric Vehicle</a></p>
-					<h4><a href="#">What You Need to Know to Get Into Electric Cars</a></h4>
-				</div><!--
-				--><a href="#" class="full-width-button read-button">Read On</a>
+		</div>
+		<div class="section pb0 pt0">
+			<div class="row">
+				<div class="small-12 large-4 columns">
+					<div class="post">
+						<div class="post-container">
+							<p><img src="images/post1.jpg" alt=""></p>
+							<p class="lead lead-small"><a href="#">Career</a></p>
+							<h4><a href="#">Confessions from Recruiters for Year 2015</a></h4>
+						</div><!--
+						--><a href="#" class="full-width-button read-button">Read On</a>
+					</div>
+				</div>
+				<div class="small-12 large-4 columns">
+					<div class="post">
+						<div class="post-container">
+							<p><img src="images/post2.jpg" alt=""></p>
+							<p class="lead lead-small"><a href="#">Design</a></p>
+							<h4><a href="#">How to Nab Those Exciting Design Jobs</a></h4>
+						</div><!--
+						--><a href="#" class="full-width-button read-button">Read On</a>
+					</div>
+				</div>
+				<div class="small-12 large-4 columns">
+					<div class="post">
+						<div class="post-container">
+							<p><img src="images/post3.jpg" alt=""></p>
+							<p class="lead lead-small"><a href="#">Electric Vehicle</a></p>
+							<h4><a href="#">What You Need to Know to Get Into Electric Cars</a></h4>
+						</div><!--
+						--><a href="#" class="full-width-button read-button">Read On</a>
+					</div>
+				</div>
 			</div>
 		</div>
 	</div>
@@ -64,7 +113,7 @@
 		 <div class="row">
 		 	<div class="small-12 columns">
 		<div class="section  pt0 text-center">
-			<p><a href="#" class="view-all-button">View All Stories</a></p>
+			<p><a href="#" class="view-all-button mobile-only">View All Stories</a></p>
 		</div>
 	</div>
 </div>
