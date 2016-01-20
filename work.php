5a54a2c637535b1268d7695ea32bc21900985262
me@diana.nu <diana@Dianas-MacBook-Air.local>
2016-01-19 19:06:47 -0800

    Work page is desktop-friendly (no js yet)

--- work.php
+++ work.php
@@ -5,58 +5,70 @@
 			<p class="lead">I'm looking for</p>
 			<h1>Work</h1>
 			<p><img src="images/line.svg" alt=""></p>
+			<div class="tal"><a href="#"><img src="images/tal.svg" alt="Talent"></a></div>
 		</div>
 	</div>
 </div>
 <div class="row">
-	<div class="small-12 columns">
-		<div class="section pt0">
-			<h4>It's about the work.</h4>
+	<div class="small-12 large-8 large-centered columns">
+		<div class="section pt0 large-text large-text-center">
+			<h4 class="large-text">It's about the work.</h4>
 			<p>Perks and bonuses are important, but we understand what truly makes an opportunity great is the work you do and the impact you make. Today's engineers and developers don't need a traditional recruiter; they need an agent.</p>
 		</div>
 	</div>
 </div>
-<div class="section0 white-section">
-	<div id="industry-dropdown-container"></div>
-	<div id="industry-content"></div>
-	<div class="design-industry industry-card card-container">
-		<div class="card">
-			<h3><a href="#">Design<br>
-			<span class="lead">Industry</span></h3>
-			<p class="mt0"><img src="images/small_line.svg" alt=""></p>
-			<h5><a href="#">Open</a></h5>
-		</div>
-	</div>
-	<div class="ev-industry industry-card card-container">
-		<div class="card">
-			<h3><a href="#">Electric Vehicle<br>
-			<span class="lead">Industry</span></h3>
-			<p class="mt0"><img src="images/small_line.svg" alt=""></p>
-			<h5><a href="#">Open</a></h5>
-		</div>
-	</div>
-	<div class="tech-industry industry-card card-container">
-		<div class="card">
-			<h3><a href="#">Technology<br>
-			<span class="lead">Industry</span></h3>
-			<p class="mt0"><img src="images/small_line.svg" alt=""></p>
-			<h5><a href="#">Open</a></h5>
+<div class="row small-collapse large-uncollapse">
+	<div class="small-12 columns">
+		<div class="section0 white-section">
+			<div id="industry-dropdown-container"></div>
+			<div id="industry-content"></div>
+				<div class="industry-cards">
+				<div class="design-industry industry-card card-container">
+					<div class="card">
+						<h3><a href="#">SBT<strong>design</strong><br>
+						<span class="lead">Industry</span></h3>
+						<p class="mt0"><img src="images/small_line.svg" alt=""></p>
+						<h5><a href="#">Open</a></h5>
+					</div>
+				</div>
+				<div class="ev-industry industry-card card-container">
+					<div class="card">
+						<h3><a href="#">SBT<strong>EV</strong><br>
+						<span class="lead">Industry</span></h3>
+						<p class="mt0"><img src="images/small_line.svg" alt=""></p>
+						<h5><a href="#">Open</a></h5>
+					</div>
+				</div>
+				<div class="tech-industry industry-card card-container">
+					<div class="card">
+						<h3><a href="#">SBT<strong>tech</strong><br>
+						<span class="lead">Industry</span></h3>
+						<p class="mt0"><img src="images/small_line.svg" alt=""></p>
+						<h5><a href="#">Open</a></h5>
+					</div>
+				</div>
+			</div>
 		</div>
 	</div>
 </div>
-<div class="section gradient-section">
-	<div class="row">
-		<div class="small-12 columns">
-			<h4>We're not your typical recruiters.</h4>
-			<p>At Silicon Beach Talent, we only partner with companies that we'd want to work for ourselves. We focus on the intangibles that make you connect with a company so we can understand what matters to you when considering a new position.</p> 
+<div class="section large-mt gradient-section large-work-bg">
+	<div class="row collapse full-width-row">
+		<div class="small-12 large-6 large-offset-6 columns">
+			<div class="work-bg-content">
+				<div class="container">
+					<h4>We're not your typical recruiters.</h4>
+					<p>At Silicon Beach Talent, we partner with fun, innovative companies who need talent to help them tackle insane challenges and meet aggressive goals. </p>
 
-			<p>Our clients are fun, innovative companies with aggressive goals and even more aggressive visions. They're not just looking for a set of skills, they're looking for someone to stand beside them to tackle insane challenges. That's why we spend time onsite with all our clients, to get to know their mission and their culture, so we know when it resonates with your career objectives.</p>
+					<p>We spend time onsite with all our clients to identify the intangibles that fortify connections.</p>
 
-			<p>We specialize in placing individual contributors and executives through our divisions listed below. In the end, our goal is simple:</p>
+					<p>We specialize in placing individual contributors and executives through our divisions with one simple goal in mind:</p>
 
-			<div class="section text-center pb0">
-				<h1 class="standalone reverse">Love<br>Your<br>Work</h1>
-				<p class="lead standalone reverse">We can help</p>
+					<div class="section small-text-center pb0">
+						<h1 class="standalone reverse">Love <br class="mobile-only">Your <br class="mobile-only">Work</h1>
+						<p class="lead standalone reverse mobile-only">We can help</p>
+						<p class="lead standalone reverse desktop-only">READY TO FIND YOUR NEXT GIG?</p>
+					</div>
+				</div>
 			</div>
 		</div>
 	</div>
