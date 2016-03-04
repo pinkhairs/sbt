7edd05a33561d9b6ef7d352177053ccc3467c2d4
Annette Wong <aaannette.w@gmail.com>
2016-03-03 20:54:33 -0500

    updated content that Max sent over for "about" and "homepage".

--- about.php
+++ about.php
@@ -24,7 +24,7 @@ include 'header.php'; ?>
 	 	<div class="small-12 columns">
 			<div class="section pt0 large-text-center">
 				<h2>A Southern California Tech Fusion</h2>
-				<p>As the flow of talent and capital to Silicon Beach increases, it's now more important than ever to focus not only on building great internal teams, but building a sustainable tech ecosystem for all of Southern California. Here’s our divisions:</p>
+				<p>As the flow of talent and capital to Silicon Beach increases, it's now more important than ever to focus not only on building great internal teams, but building a sustainable tech ecosystem for all of Southern California.  We do this through our three recruitment divisions:</p>
 				<div class="dropdown-container">
 					<div class="dropdown-label">Work</div>
 					<ul class="dropdown">
@@ -40,7 +40,7 @@ include 'header.php'; ?>
 	 	<div class="small-12 large-7 end columns">
 			<div class="section pt0">
 				<h2>Culture fit + Inspiring Work = A Big Impact</h2>
-				<p>Our recruiters work onsite to thoroughly understand the culture of your company so we can talk to exceptional candidates about what really matters to them: the work.</p>
+				<p>Our recruiters work onsite to thoroughly understand the culture of your company so we can talk to exceptional candidates about what really matters to them: the work. </p>
 
 				<p>Perks and bonuses are important, but we understand that what truly makes an opportunity great is the work you do and the impact you make. </p>
 
@@ -51,22 +51,22 @@ include 'header.php'; ?>
 	 <div class="row">
 	 	<div class="small-12 large-7 end columns">
 			<div class="section pt0">
-				<h2>Fresh Recruitment Style</h2>
-				<p>We're not your typical recruiters. At Silicon Beach Talent, we only partner with companies that we'd want to work for ourselves.</p>
+				<h2>Not Your Typical Recruiters</h2>
+				<p>At Silicon Beach Talent, we only partner with companies that we would want to work for ourselves. </p>
 
 				<p>We focus on the intangibles that make you connect with a company so we can understand what matters to you when considering a new position. </p>
 
 				<p>Our partners are fun, innovative companies with aggressive goals and even more aggressive visions. </p>
 
-				<p>They're not just looking for a set of skills, they're looking for someone to stand beside them to tackle insane challenges.</p>
+				<p>They are not just looking for a set of skills, they are looking for someone to stand beside them to tackle insane challenges. </p>
 			</div>
 		</div>
 	</div>
 	 <div class="row">
 	 	<div class="small-12 large-7 text-center large-centered columns">
 			<div class="section pt0">
-				<h2>Recruiting and Placement That Tells the Whole Story</h2>
-				<p>You want the complete picture, that's why we spend time onsite with all our partners. We understand every client’s mission and culture, so we know when it resonates with your career objectives. </p>
+				<h2>It's about the IMPACT</h2>
+				<p>There is a lot of noise in the job market with the rise of anonymous job matching sites, but they are all missing the point.  You want the whole picture, not just a quick description and a salary estimate.  That's why we spend time onsite with all our partners. We understand every client’s mission and culture, so we know when it resonates with your career objectives. </p>
 	 
 				<p>In the end, our goal is simple: </p>
 				<div class="section small-text-center pb0 small-love">
