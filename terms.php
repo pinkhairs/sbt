063486b86e5785f4cfff61d7fcde58eb5d5b8a11
Annette Wong <aaannette.w@gmail.com>
2016-03-03 21:48:33 -0500

    added terms and privacy policy

---
+++ terms.php
@@ -0,0 +1,133 @@
+<?php 
+// Define variables for SEO
+$pageName = 'generic';
+$pageBodyClass = $pageName;
+$pageTitle = 'Silicon Beach Talent | Terms and Conditions';
+$pageDescription = 'We are a recruiting and consulting firm matching skills, company culture and personal connections while building a sustainable tech ecosystem in Southern California.';
+$pageKeywords = 'silicon, beach, recruiting, california, southern california, los angeles, san diego, design, developer, electric vehicle';
+$pageCanonical = 'http://www.siliconbeachtalent.com';
+// $pageRobots = 'noindex,nofollow';
+
+include 'header.php'; ?>
+
+<div class="generic-template-container">
+   <div class="row">
+    <div class="small-12 columns">
+      <div class="section medium-section text-center">
+        <h1>TERMS OF USE</h1>
+        <p class="lead mt0"></p>
+        <p><img src="images/line.svg" alt=""></p>
+      </div>
+    </div>
+  </div>
+  <div class="row">
+    <div class="small-12 large-8 large-offset-2 columns">
+      <div class="section pt0">
+        <p> Welcome to our Website. By using our site, you are agreeing to comply with and be bound by the following terms of use. Please review the following terms carefully. If you do not agree to these terms, you should not use this site. The term “company” or “us” or “we” or “our” refers to Silicon Beach Talent LLC, the owner of the Website. The term “you” refers to the user or viewer of our Website.</p>
+
+        <h2>1. Acceptance of Agreement.</h2>
+        <p>You agree to the terms and conditions outlined in this Terms of Use Agreement ("Agreement") with respect to our site (the "Site"). This Agreement constitutes the entire and only agreement between us and you, and supersedes all prior or contemporaneous agreements, representations, warranties and understandings with respect to the Site, the content, products or services provided by or through the Site, and the subject matter of this Agreement. This Agreement may be amended at any time by us from time to time without specific notice to you. The latest Agreement will be posted on the Site, and you should review this Agreement prior to using the Site.</p>
+
+        <h2>2. Copyright.</h2>
+        <p>The content, organization, graphics, design, compilation, magnetic translation, digital conversion and other matters related to the Site are protected under applicable copyrights, trademarks and other proprietary (including but not limited to intellectual property) rights. The copying, redistribution, use or publication by you of any such matters or any part of the Site, except as allowed by Section 4 below, is strictly prohibited. You do not acquire ownership rights to any content, document or other materials viewed through the Site. The posting of information or materials on the Site does not constitute a waiver of any right in such information and materials. Some of the content on the site is the copyrighted work of third parties.</p>
+
+        <h2>3. Service Marks.</h2>
+        <p>"Silicon Beach Talent" and others are our service marks or registered service marks or trademarks. Other product and company names mentioned on the Site may be trademarks of their respective owners. All trademarks, trade names, service marks, and logos mentioned herein are the property of their respective owners.</p>
+
+        <h2>4. Limited License; Permitted Uses.</h2>
+        <p>You are granted a non-exclusive, non-transferable, revocable license (a) to access and use the Site strictly in accordance with this Agreement; (b) to use the Site solely for internal, personal, non-commercial purposes; and (c) to print out discrete information from the Site solely for internal, personal, non-commercial purposes and provided that you maintain all copyright and other policies contained therein. No print out or electronic version of any part of the Site or its contents may be used by you in any litigation or arbitration matter whatsoever under any circumstances.</p>
+
+        <h2>5. Restrictions and Prohibitions on Use.</h2>
+        <p>Your license for access and use of the Site and any information, materials or documents (collectively defined as “Content and Materials”) therein are subject to the following restrictions and prohibitions on use: You may not (a) copy, print (except for the express limited purpose permitted by Section 4 above), republish, display, distribute, transmit, sell, rent, lease, loan or otherwise make available in any form or by any means all or any portion of the Site or any Content and Materials retrieved therefrom; (b) use the Site or any materials obtained from the Site to develop, of as a component of, any information, storage and retrieval system, database, information base, or similar resource (in any media now existing or hereafter developed), that is offered for commercial distribution of any kind, including through sale, license, lease, rental, subscription, or any other commercial distribution mechanism; (c) create compilations or derivative works of any Content and Materials from the Site; (d) use any Content and Materials from the Site in any manner that may infringe any copyright, intellectual property right, proprietary right, or property right of us or any third parties; (e) remove, change or obscure any copyright notice or other proprietary notice or terms of use contained in the Site; (f) make any portion of the Site available through any timesharing system, service bureau, the Internet or any other technology now existing or developed in the future; (g) remove, decompile, disassemble or reverse engineer any Site software or use any network monitoring or discovery software to determine the Site architecture; (h) use any automatic or manual process to harvest information from the Site; (i) use the Site for the purpose of gathering information for or transmitting (1) unsolicited commercial email; (2) email that makes use of headers, invalid or nonexistent domain names, or other means of deceptive addressing; and (3) unsolicited telephone calls or facsimile transmissions; (j) use the Site in a manner that violates any state or federal law regulating email, facsimile transmissions or telephone solicitations; and (k) export or re-export the Site or any portion thereof, or any software available on or through the Site, in violation of the export control laws or regulations of the United States.</p>
+
+        <h2>6. Errors, Corrections and Changes.</h2>
+        <p>We do not represent or warrant that the Site will be error-free, free of viruses or other harmful components, or that defects will be corrected. We do not represent or warrant that the information available on or through the Site will be correct, accurate, timely or otherwise reliable. We may make changes to the features, functionality or content of the Site at any time. We reserve the right in our sole discretion to edit or delete any documents, information or other content appearing on the Site.</p>
+
+        <h2>7. Indemnification.</h2>
+        <p>You agree to indemnify, defend and hold us and our partners, agents, officers, directors, employees, subcontractors, successors, assigns, third party suppliers of information and documents, attorneys, advertisers, product and service providers, and affiliates (collectively, "Affiliated Parties") harmless from any liability, loss, claim and expense, including reasonable attorney's fees, related to your violation of this Agreement or use of the Site.</p>
+
+        <h2>8. Nontransferable.</h2>
+        <p>Your right to use the Site is not transferable or assignable. Any password or right given to you to obtain information or documents is not transferable or assignable.</p>
+
+        <h2>9. Disclaimer.</h2>
+        <p>THE INFORMATION FROM OR THROUGH THIS SITE IS PROVIDED “AS-IS,” “AS AVAILABLE,” AND ALL WARRANTIES, EXPRESS OR IMPLIED, ARE DISCLAIMED (INCLUDING BUT NOT LIMITED TO THE DISCLAIMER OF ANY IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE).</p>
+        <p>WE ARE NOT LIABLE FOR ANY INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES (INCLUDING DAMAGES FOR LOSS OF BUSINESS, LOSS OF PROFITS, OR THE LIKE). WHETHER BASED ON BREACH OF CONTRACT, BREACH OF WARRANTY, TORT (INCLUDING NEGLIGENCE), PRODUCT LIABILITY OR OTHERWISE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. THE LIMITATIONS OF DAMAGES SET FORTH ABOVE ARE FUNDAMENTAL ELEMENTS OF THE BASIS OF THE BARGAIN BETWEEN US AND YOU. WE WOULD NOT PROVIDE THIS SITE AND INFORMATION WITHOUT SUCH LIMITATIONS. NO REPRESENTATIONS, WARRANTIES OR GUARANTEES WHATSOEVER ARE MADE AS TO THE ACCURACY, ADEQUACY, RELIABILITY, CURRENTNESS, COMPLETENESS, SUITABILITY OR APPLICABILITY OF THE INFORMATION TO A PARTICULAR SITUATION.</p>
+
+        <h2>10. Limitation of Liability.</h2>
+        <p>(a) We and any Affiliated Party shall not be liable for any loss, injury, claim, liability, or damage of any kind resulting in any way from (a) any errors in or omissions from the Site or any services or products obtainable therefrom, (b) the unavailability or interruption of the Site or any features thereof, (c) your use of the Site, (d) the content contained on the Site, or (e) any delay or failure in performance beyond the control of a Covered Party.</p>
+
+        <h2>11. Use of Information.</h2>
+        <p>We reserve the right, and you authorize us, to the use and assignment of all information regarding Site uses by you and all information provided by you in any manner consistent with our Privacy Policy. All remarks, suggestions, ideas, graphics, or other information communicated by you to us (collectively, a "Submission") will forever be our property. We will not be required to treat any Submission as confidential, and will not be liable for any ideas (including without limitation, product, service or advertising ideas) and will not incur any liability as a result of any similarities that may appear in our future products, services or operations. Without limitation, we will have exclusive ownership of all present and future existing rights to the Submission of every kind and nature everywhere. We will be entitled to use the Submission for any commercial or other purpose whatsoever, without compensation to you or any other person sending the Submission. You acknowledge that you are responsible for whatever material you submit, and you, not us, have full responsibility for the message, including its legality, reliability, appropriateness, originality, and copyright.</p>
+
+        <h2>12. Privacy Policy.</h2>
+        <p>Our Privacy Policy, as it may change from time to time, is a part of this Agreement. You must review this <a href="http://www.altonmorgan.com/privacy.htm">Privacy Policy</a> by clicking on this link.</p>
+
+        <h2>13. Links to other Websites.</h2>
+        <p>The Site contains links to other Websites. We are not responsible for the content, accuracy or opinions express in such Websites, and such Websites are not investigated, monitored or checked for accuracy or completeness by us. Inclusion of any linked Website on our Site does not imply approval or endorsement of the linked Website by us. If you decide to leave our Site and access these third-party sites, you do so at your own risk.</p>
+
+        <h2>14. Information and Press Releases.</h2>
+        <p>The Site contains information and press releases about us. We disclaim any duty or obligation to update this information or any press releases. Information about companies other than ours contained in the press release or otherwise, should not be relied upon as being provided or endorsed by us.</p>
+
+        <h2>15. Legal Compliance.</h2>
+        <p>You agree to comply with all applicable domestic and international laws, statutes, ordinances and regulations regarding your use of the Site and the Content and Materials provided therein.</p>
+
+        <h2>16. Miscellaneous.</h2>
+        <p>This Agreement shall be treated as though it were executed and performed in Los Angeles County, California, and shall be governed by and construed in accordance with the laws of the State of California (without regard to conflict of law principles). Any cause of action by you with respect to the Site (and/or any information or services related thereto) must be instituted within one (1) year after the cause of action arose or be forever waived and barred. All actions shall be subject to the limitations set forth in Section 9 and Section 10. The language in this Agreement shall be interpreted as to its fair meaning and not strictly for or against any party. This Agreement and all incorporated agreements and your information may be automatically assigned by us in our sole discretion to a third party in the event of an acquisition, sale or merger. Should any part of this Agreement be held invalid or unenforceable, that portion shall be construed consistent with applicable law and the remaining portions shall remain in full force and effect. To the extent that anything in or associated with the Site is in conflict or inconsistent with this Agreement, this Agreement shall take precedence. Our failure to enforce any provision of this Agreement shall not be deemed a waiver of such provision nor of the right to enforce such provision. Our rights under this Agreement shall survive any termination of this Agreement.</p>
+
+        <h2>17. Arbitration.</h2>
+        <p>Any legal controversy or legal claim arising out of or relating to this Agreement or our services, excluding legal action taken by us to collect or recover damages for, or obtain any injunction relating to, Site operations, intellectual property, and our services, shall be settled solely by binding arbitration in accordance with the commercial arbitration rules of JAMS. Any such controversy or claim shall be arbitrated on an individual basis, and shall not be consolidated in any arbitration with any claim or controversy of any other party. The arbitration shall be conducted in Los Angeles, California, and judgment on the arbitration award may be entered into any court having jurisdiction thereof. Either you or us may seek any interim or preliminary relief from a court of competent jurisdiction in Los Angeles, California necessary to protect the rights or property of you and us pending the completion of arbitration. Each party shall bear one-half of the arbitration fees and costs incurred through JAMS.</p>
+
+      </div>
+    </div>
+  </div>
+  <div class="row">
+    <div class="small-12 columns">
+      <div class="section medium-section text-center">
+        <h1>Privacy Policy</h1>
+        <p class="lead mt0"></p>
+        <p><img src="images/line.svg" alt=""></p>
+      </div>
+    </div>
+  </div>
+  <div class="row">
+    <div class="small-12 large-8 large-offset-2 columns">
+      <div class="section pt0">
+        <p> Our Privacy Policy is designed to assist you in understanding how we collect and use the personal information you provide to us and to assist you in making informed decisions when using our site and our services.</p>
+
+        <h3>What Information Do We Collect?</h3>
+        <p>When you visit our Website you may provide us with two types of information: personal information you knowingly choose to disclose that is collected on an individual basis and use information collected on an aggregate basis as you and others browse our Website.</p>
+
+        <h2>1. Personal Information You Choose to Provide</h2>
+        <p>You may provide us information about yourself, your firm or company, and your practices when you communicate with us through a Web form or email. You may also provide additional comments on how you see Silicon Beach Talent servicing your needs and interests.</p>
+
+        <h3>Email Information.</h3>
+        <p>If you choose to correspond with us through email, we may retain the content of your email messages together with your email address and our responses.</p>
+
+        <h3>Confidential Job Seekers.</h3>
+        <p>In order to present your qualifications to potential employers, you agree that your personal information may be shared with our clients and affiliates.</p>
+
+        <h2>2. Website Use Information</h2>
+        <p>Similar to other commercial Websites, our Website utilizes standard Web server logs that include information such as the date and time of visits, the pages viewed, time spent at our Website, etc.</p>
+
+        <h3>How Do We Use the Information We Collect?</h3>
+        <p>Broadly speaking, we use personal information for purposes of administering and expanding our business activities, providing customer service and making available other services to job seekers and clients. Occasionally, we may also use the information we collect to notify you about important changes to our Website, new services and special offers we think you will find valuable. You may notify us at any time if you do not wish to receive these offers by emailing us at info@siliconbeachtalent.com.</p>
+
+        <h3>How Do We Protect Your Information?</h3>
+        <p>Email is not recognized as a secure medium of communication. For this reason, we request that you do not send private information to us by email.</p>
+
+        <p>We are not responsible for the practices employed by Websites linked to or from our Website nor the information or content contained therein. Often links to other Websites are provided solely as pointers to information on topics that may be useful to the users of our Website.</p>
+
+        <p>Please remember that when you use a link to go from our Website to another Website, our Privacy Policy is no longer in effect. Your browsing and interaction on any other Website, including Websites which have a link on our Website, is subject to that Website's own rules and policies. Please read over those rules and policies before proceeding.</p>
+
+        <h3>Certain Disclosures</h3>
+        <p>We may disclose your personal information if required to do so by law or subpoena or if we believe that such action is necessary to (a) conform to the law or comply with legal process served on us or affiliated parties; (b) protect and defend our rights and property, our site, the users of our site, and/or our affiliated parties; (c) act under circumstances to protect the safety of users of our site, us, or third parties.</p>
+
+        <p>We may also share your information with our partners and affiliates that are committed to serving your needs as a job seeker or client, but they will be required to adhere to our privacy policy.</p>
+
+        <h3>Your Consent</h3>
+        <p>By using our Website you consent to our collection and use of your personal information as described in this Privacy Policy. If we change our privacy policies and procedures, we will post those changes on our Website to keep you aware of what information we collect, how we use it and under what circumstances we may disclose it.</p>
+
+        </div>
+    </div>
+  </div>
+<?php include 'footer.php'; ?>
