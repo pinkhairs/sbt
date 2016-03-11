389b756b2a70fc04c6bc535c6af0e34fac1a493e
Annette Wong <aaannette.w@gmail.com>
2016-03-11 00:05:14 -0500

    added google analytics code to the footer for SBT

--- blog/wp-content/themes/sbt/footer.php
+++ blog/wp-content/themes/sbt/footer.php
@@ -41,6 +41,17 @@
 					<a href="#">Terms &amp; Conditions</a></span>
 				</div>
 			</div>
+
+		<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-74978027-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
 		</footer>
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js" type="text/javascript"></script>
 <?php if ($_COOKIE['preload'] != 1) { ?>
