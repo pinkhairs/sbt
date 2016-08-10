39f2bfce3877cb46c9a8cac39f9ae6f20505c7fc
Chris Porter <general@madebyporter.com>
2016-08-09 18:36:41 -0700

    added referral page text

--- contactus/wp-content/themes/sbt/page-referrals.php
+++ contactus/wp-content/themes/sbt/page-referrals.php
@@ -11,11 +11,18 @@ Template Name: Referrals
   <?php while(have_posts()) : the_post(); ?>
   <div class="blog-index">
     <div class="row">
-      <div class="small-12 columns">
+      <div class="small-12 large-8 large-offset-2 columns large-text">
         <div class="section medium-section text-center">
           <h1>Referral Program</h1>
           <p class="lead mt0">Silicon Beach Talent</p>
           <p><img src="../../images/line.svg" alt=""></p>
+        </div>
+        <div class="section pt0 large-text-center">
+          <p>“Great people know great people.”  It is a phrase almost as cliché as it is true.  At Silicon Beach Talent, our network on and our professional reputation is one and the same.  We believe in forming true partnerships, which is why we offer a referral bonus if we place someone that you introduce us to.</p>
+
+          <p>We are not one of those “throw as many referrals at the wall and see what sticks” companies though.  Our clients depend on us to submit highly qualified and exceptionally talented individuals, and our candidates depend on us to tell them about a potential opportunity in-depth confidentially, so they have the information they need to decide whether they are interested.</p>
+
+          <p>If you have someone you think we should speak with, please contact us using the form below and we will go from there.</p>
         </div>
       </div>
     </div>
